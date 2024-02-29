539651d5cebb372b50f85cd28859d5559760dcf1
F-Shouter <132644949+F-Shouter@users.noreply.github.com>
2024-02-28 21:15:40 -0300

    update

--- Java/Proj_Equacao2Grau/src/proj_equacao2grau/Proj_Equacao2Grau.java
+++ Java/Proj_Equacao2Grau/src/proj_equacao2grau/Proj_Equacao2Grau.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package proj_equacao2grau;
-
+import java.util.Scanner;
 /**
  *
  * @author Fujoshi Shouter
@@ -14,11 +14,30 @@ public class Proj_Equacao2Grau {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Equacao2Grau eq1 = new Equacao2Grau(1,-5,6);
+        
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite o valor de A: ");
+        double ca = entrada.nextDouble();
+        
+        System.out.print("Digite o valor de B: ");
+        double cb = entrada.nextDouble();
+        
+        System.out.print("Digite o valor de C: ");
+        double cc = entrada.nextDouble();
+        
+        
+        Equacao2Grau eq1 = new Equacao2Grau(ca, cb, cc);
         eq1.calcularX1();
         eq1.calcularX2();
-        eq1.imprimir();
+       // eq1.imprimir();
+       
+       if ( eq1.calcularDelta() > 0 ){
+            System.out.println("x1= "+ eq1.getX1());
+            System.out.println("x2= "+ eq1.getX2());
+       }
+       else {
+           System.out.println("As raízes são complexas, por isso não quero mostrá-las");
+       }
     }
     
 }
