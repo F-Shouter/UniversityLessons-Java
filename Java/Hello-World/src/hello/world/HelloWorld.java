330a39d680b87f59f42812228776a5e4e5e93e5f
Yamasaki Yukari <132644949+F-Shouter@users.noreply.github.com>
2024-02-28 21:56:43 -0300

    Update HelloWorld.java

--- Java/Hello-World/src/hello/world/HelloWorld.java
+++ Java/Hello-World/src/hello/world/HelloWorld.java
@@ -4,8 +4,6 @@
  */
 package hello.world;
 
-import java.util.Scanner;
-
 /**
  *
  * @author Fujoshi Shouter
@@ -16,18 +14,9 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
         System.out.println("Hello-World");
-        Scanner entrance = new Scanner (System.in);
         
-        System.out.print("Enter an integer: ");
-        int v1 = entrance.nextInt();
-        
-        System.out.print("Enter another integer: ");
-        int v2 = entrance.nextInt();
-        
-        int soma = v1 + v2;
-        System.out.println("The sum of two values is: " + soma);
     }
     
 }
