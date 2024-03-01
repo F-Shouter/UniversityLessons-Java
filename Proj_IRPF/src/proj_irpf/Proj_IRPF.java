55533f076161e19b5515fbaf0ab10d5b51360e21
F-Shouter <132644949+F-Shouter@users.noreply.github.com>
2024-02-29 22:07:45 -0300

    Added Proj_IRPF

--- Proj_IRPF/src/proj_irpf/Proj_IRPF.java
+++ Proj_IRPF/src/proj_irpf/Proj_IRPF.java
@@ -14,6 +14,7 @@ public class Proj_IRPF {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("a");
         // TODO code application logic here
     }
     
