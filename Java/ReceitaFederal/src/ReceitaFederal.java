bb095190a4874c0d9c8dc64403aa5a35713d7df4
F-Shouter <132644949+F-Shouter@users.noreply.github.com>
2024-03-11 21:55:29 -0300

    new folder

---
+++ Java/ReceitaFederal/src/ReceitaFederal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.receitafederal;
+
+/**
+ *
+ * @author Fujoshi Shouter
+ */
+public class ReceitaFederal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
